60201f22938a1114c8a71e57222169f1af914432
kitamura-hub <masato1999824@icloud.com>
2020-09-23 17:22:37 +0900

    add .tmux.conf

--- link.sh
+++ link.sh
@@ -3,6 +3,6 @@ ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 
