f308e8c07f58db7cfc27d1e4350f63ae009b71a4
kitamura-hub <masato1999824@icloud.com>
2020-09-27 17:40:40 +0900

    add install extention

--- .vscode/vscode_link.sh
+++ .vscode/vscode_link.sh
@@ -1,4 +1,11 @@
- #!/bin/zsh
- ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
- ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json 
+#!/bin/zsh
+ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json 
 
+# install extention
+cat extensions | while read line
+do
+ code --install-extension $line
+done
+
+code --list-extensions > extensions
