0f02234e6739e52f4219314a916fc6d4481ba38d
kitamura-hub <masato1999824@icloud.com>
2020-09-27 17:24:14 +0900

    add vscode

---
+++ .vscode/vscode_link.sh
@@ -0,0 +1,4 @@
+ #!/bin/zsh
+ ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json 
+
