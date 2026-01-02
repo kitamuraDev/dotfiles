45ec28998c970f4b5f9c1de7157ca3ddc691c027
kitamuraDev <masato1999824@icloud.com>
2026-01-02 16:35:39 +0900

    :wrench: flash.nvimのconfigを訂正（誤ったキーマップをしてしまっていた）

--- packages/nvim/.config/nvim/lua/plugins/flash-nvim.lua
+++ packages/nvim/.config/nvim/lua/plugins/flash-nvim.lua
@@ -3,11 +3,12 @@ return {
   event = "VeryLazy",
   ---@type Flash.Config
   opts = {},
-  keys = {
-    { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "F", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+  config = function()
+    require("flash").setup({})
+    vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+    vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+    vim.keymap.set({ "o" }, "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+    vim.keymap.set({ "x", "o" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+    vim.keymap.set({ "c" }, "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
+  end
 }
